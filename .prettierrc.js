5e9dfd08a2956c8a060c52b89967b6acc92b0a1d
keithmccall <keithcodes@gmail.com>
2024-05-06 20:57:11 -0700

    Starter

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  plugins:[require.resolve('prettier-plugin-organize-imports')]
+};
